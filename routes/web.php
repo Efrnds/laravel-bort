130291268a07f7d4be6722a9b4c6c1a340d86f8f
efrnds <eduardo.fernandes.silva13@gmail.com>
2025-08-25 20:17:39 -0300

    vai tomar no cu laravel

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/status', function () {
+    return 'OK';
+});
 
 Route::get('cu', function () {
     return view('cu');
@@ -25,4 +28,15 @@ Route::get('cu/{v1}/{v2}', function ($v1, $v2) {
 
 Route::get('adicionar-produto/{param1}/{param2}', function ($param1, $param2) {
     return "Produto adicionado com sucesso! Parâmetro 1: {$param1}, Parâmetro 2: {$param2}";
+});
+
+Route::get('/series', function () {
+    $series = ['Breaking Bad', 'Game of Thrones', 'Stranger Things', 'The Crown'];
+
+    $html = "<ul>";
+    foreach ($series as $serie) {
+        $html .= "<li>{$serie}</li>";
+    }
+    $html .= "</ul>";
+    return $html;
 });
